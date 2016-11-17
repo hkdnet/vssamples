2a603532d26143e711e09f2d9a7e80208f36633f
hkdnet <hkdnet@users.noreply.github.com>
2016-11-17 22:35:20 +0900

    init

---
+++ vssamples/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		var arr = new int[] { 1, 2, 3 };
+		foreach (var item in arr.Select(x => x * 2))
+		{
+			Console.WriteLine(item);
+		}
+		Console.WriteLine("Hello World!");
+	}
+}
